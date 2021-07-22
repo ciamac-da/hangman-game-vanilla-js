a6f6ed83f8a8ec84aa6b5ce7a600cd3f8e1139aa
ciamac-da <ciamac.davoudi@gmail.com>
2021-07-22 22:38:01 +0200

    Verified by Marti4n v1.0.7 wrong letter

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const finalMessage = document.getElementById("final-message")
 const figureParts = document.querySelectorAll(".figure-part")
 
 const words = ["application", "programming", "wizard", "javascript"]
-const selectedWord = words[Math.floor(Math.random() * words.length)]
+let selectedWord = words[Math.floor(Math.random() * words.length)]
 
-const correctLetters = []
-const wrongLetters = []
+let correctLetters = []
+let wrongLetters = []
 
 // Show hidden word
 const displayWord = () => {
@@ -35,7 +35,46 @@ const displayWord = () => {
 
 // Update the wrong letters
 const updateWrongLettersEl = () => {
-  console.log("Update wrong")
+
+ // Hide display wrong 
+  if(wrongLetters.length === 0) {
+    wrongLetters.map(letter => `<span>${letter}</span>`)
+    wrongLetterEl.innerHTML = `${wrongLetters}`
+  }
+
+ // Display wrong letter
+  if(wrongLetters.length > 0) {
+    wrongLetters.map(letter => `<span>${letter}</span>`)
+    wrongLetterEl.innerHTML = `
+    <p>Wrong Letter</p>
+     ${wrongLetters}
+    `
+  }
+  // Display wrong letters
+   if(wrongLetters.length > 1 ) {
+    wrongLetters.map(letter => `<span>${letter}</span>`)
+    wrongLetterEl.innerHTML = `
+    <p>Wrong Letters</p>
+    ${wrongLetters}
+    `
+
+    // Check if Lost
+    if (wrongLetters.length === figureParts.length) {
+      finalMessage.innerText = "Unfortunately you lost"
+      popup.style.display = "flex"
+    }
+  }
+
+// Display Parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length
+
+    if(index < errors) {
+      part.style.display = "block"
+    } else {
+      part.style.display = "none"
+    }
+  })
 }
 
 // Show notification
@@ -51,7 +90,6 @@ const showNotification = () => {
 
 // Keydown letter press
 window.addEventListener("keydown", e => {
-  console.log(e.keyCode)
   if(e.keyCode >= 65 && e.keyCode <= 90) {
     const letter = e.key;
     if(selectedWord.includes(letter)) {
@@ -64,12 +102,23 @@ window.addEventListener("keydown", e => {
     } else {
       if(!wrongLetters.includes(letter)) {
         wrongLetters.push(letter)
-        updateWrongLetterEl()
+        updateWrongLettersEl()
       } else {
         showNotification()
       }
     }
   }
 })
+
+// Restart game and play again
+playAgainBtn.addEventListener("click", () => {
+  // Empty Arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+  displayWord();
+  updateWrongLettersEl();
+  popup.style.display = "none"
+});
 
 displayWord()
